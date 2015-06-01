f23cac0e4757dbc67d0e724f4d3609fc4b03e23d
Kazim Mehdi <kmehdi@gmail.com>
2015-06-01 06:07:56 -0500

    committing changes

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -116,6 +116,7 @@ angular.module('starter', ['ionic', 'ngAutocomplete','starter.controllers', 'sta
                 url: '/booking',
                 views: {
                     'mainContent': {
+                        controller: 'BookingCtrl',
                         templateUrl: 'templates/booking.html'
                     }
                 }
@@ -160,10 +161,7 @@ angular.module('starter', ['ionic', 'ngAutocomplete','starter.controllers', 'sta
             .state('home', {
                 url: '/index',
                 templateUrl: 'index.html'
-            })
-
-
-        ;
+            });
 
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/sidemenu/login');
